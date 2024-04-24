17c560caafac9a3972cbb8d65aca5ad4925df888
Roman-Awale <romanawale@gmail.com>
2024-04-24 16:42:57 +0545

    modification

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -3,10 +3,10 @@ import anime from "animejs/lib/anime.es.js";
 import Draggable from "react-draggable";
 import { Resizable } from "react-resizable";
 
-const MyComponent = () => {
+const MyComponent = (props) => {
   const [isOpen, setIsOpen] = useState(false);
   const [position, setPosition] = useState(null);
-
+  const [minWidth, setminWidth] = useState(200);
   const handleOpen = () => {
     setIsOpen(true);
   };
@@ -16,13 +16,13 @@ const MyComponent = () => {
 
     anime({
       targets: ".my-component .el",
+      // translateX: buttonRect.left - position.x,
+      // translateY: buttonRect.top - position.y,
       overflow: "hidden",
       minWidth: 0,
       minHeight: 0,
       height: 20,
       width: 45,
-      translateX: buttonRect.left - position.x,
-      translateY: buttonRect.top - position.y,
       duration: 300,
       easing: "easeOutQuad",
       complete: () => {
@@ -34,7 +34,7 @@ const MyComponent = () => {
   };
 
   const handleDrag = (e, data) => {
-    setPosition({ x: data.x, y: data.y });
+    // setPosition({ x: data.x, y: data.y });
   };
 
   const triggerRef = useRef(null);
@@ -42,7 +42,7 @@ const MyComponent = () => {
   useEffect(() => {
     if (triggerRef.current !== null && !isOpen) {
       const rect = triggerRef.current.getBoundingClientRect();
-      setPosition({ x: rect.left, y: rect.top });
+      setPosition({ x: rect.left - minWidth, y: rect.top });
     }
   }, [isOpen]);
 
@@ -63,21 +63,22 @@ const MyComponent = () => {
     }
   }, [isOpen]);
 
+  console.log(triggerRef);
+
   return (
-    <div className="my-component">
-      <button onClick={handleOpen} ref={triggerRef}>
-        open
+    <div className="my-component" style={props.wrapperStyles}>
+      <button onClick={handleOpen} ref={triggerRef} style={props.triggerStyles}>
+        {props.openText || "open"}
       </button>
 
       {isOpen && (
         <Draggable
           axis="both"
           handle=".handle"
-          defaultPosition={{ x: position.x, y: position.y }}
           bounds="body"
           onDrag={handleDrag}
         >
-          <Resizable width={10} height={10}>
+          <Resizable>
             <div
               className="component-container el"
               style={{
@@ -85,12 +86,17 @@ const MyComponent = () => {
                 resize: "both",
                 overflow: "auto",
                 transitionTimingFunction: "ease-out",
+                left: position.x,
+                minWidth: minWidth,
+                top: position.y,
               }}
             >
-              <div className="handle">
+              <div className="handle" style={props.handleStyles}>
                 <button onClick={handleClose}>close</button>
               </div>
-              <span>Contents</span>
+              <div style={props.contentWrapperStyles}>
+                {props.children || "content"}
+              </div>
             </div>
           </Resizable>
         </Draggable>
